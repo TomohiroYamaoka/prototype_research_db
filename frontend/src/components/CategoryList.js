e93085b86f386b7a71dc6c1b48e37df06cb5e041
山岡智裕 <>
2021-11-13 09:52:33 +0900

    ディレクトリ作成

---
+++ frontend/src/components/CategoryList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const CategoryList = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/daily/category/univ">大学</Link>
+        </li>
+        <li>
+          <Link to="/daily/category/study">勉強</Link>
+        </li>
+        <li>
+          <Link to="/daily/category/other">その他</Link>
+        </li>
+        <li>
+          <Link to="/daily/category/first_meet">初めましてだったこと</Link>
+        </li>
+        <li>
+          <Link to="/daily/category/univ">やりたいこと</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
