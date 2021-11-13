8e6af7f08b7b157f64744c50c51a4abcb229ca7d
山岡智裕 <>
2021-11-13 14:55:58 +0900

    apiを受け取り表示させる

---
+++ frontend/src/pages/DailyTop.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { getDaily } from "../daily/api/getDaily";
+import { DailyContent } from "../components/DailyContent";
+import { CategoryList } from "../components/CategoryList";
+
+export const DailyTop = () => {
+  const initialState = {
+    id: "",
+    date: "",
+    evaluation: "",
+  };
+
+  const [daily, setDaily] = useState(initialState);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getDaily()
+      .then((d) => {
+        setDaily(d);
+        setLoading(false);
+      })
+      .catch((e) => {
+        throw new Error(e);
+      });
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>loading...</h1>
+      ) : (
+        <div>
+          {daily.map((d) => (
+            <DailyContent {...d} />
+          ))}
+        </div>
+      )}
+      <CategoryList />
+    </div>
+  );
+};
